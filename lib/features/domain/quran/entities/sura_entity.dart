464a8b59f74bc5013bd65813f16f4ef5ed64b29d
fadyk965-ai <fadyk965@gmail.com>
2026-08-09 22:51:34 +0300

    Create SurahEntity class for Quran data model

---
+++ lib/features/domain/quran/entities/sura_entity.dart
@@ -0,0 +1,15 @@
+class SurahEntity {
+  final int number;
+  final String name;
+  final String englishName;
+  final int numberOfAyahs;
+  final String revelationType;
+
+  const SurahEntity({
+    required this.number,
+    required this.name,
+    required this.englishName,
+    required this.numberOfAyahs,
+    required this.revelationType,
+  });
+}
